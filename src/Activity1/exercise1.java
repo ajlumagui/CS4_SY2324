f422ab9ff11ff7be062b95cb7f049552ccbcec0a
TRUTH <TRUTH@172.22.16.63>
2023-09-08 16:13:59 +0800

    hiiii

--- src/Activity1/exercise1.java
+++ src/Activity1/exercise1.java
@@ -11,4 +11,43 @@ package Activity1;
  */
 public class exercise1 {
     
+    public static void main(String[] args){
+        String char1 = "Seele";
+        String element1 = "Quantum";
+        int release1 = 137;    
+    
+        String char2 = "Kafka";
+        String element2 = "Lightning";
+        int release2 = 31;
+    
+        String char3 = "Dan Heng: Imbibitor Lunae";
+        String element3 = "Imaginary";
+        int release3 = 10;
+    
+        System.out.println("Character 1");
+        System.out.println("Name: " + char1);
+        System.out.println("Element: " + element1);
+        System.out.println("Days since release: " + release1);
+    
+        System.out.println("Character 2");
+        System.out.println("Name: " + char2);
+        System.out.println("Element: " + element2);
+        System.out.println("Days since release: " + release2);
+    
+        System.out.println("Character 3");
+        System.out.println("Name: " + char2);
+        System.out.println("Element: " + element2);
+        System.out.println("Days since release: " + release2);
+    
+        System.out.println("Total days released: " + (release1 + release2 + release3));
+        
+        System.out.print("Seele is the earliest released: ");
+        if(release1>release2+release3){
+                System.out.println("True");
+        } else System.out.println("False");
+        System.out.print("The characters all have the same element: ");
+        if(element1==element2){
+            System.out.print("True");
+        } else System.out.print("False");
+    }
 }
